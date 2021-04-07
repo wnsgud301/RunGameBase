43543f69b603332ed6b61a9b8d723e38a3785575
wnsgud301 <80800106+wnsgud301@users.noreply.github.com>
2021-04-07 11:22:22 +0900

    4월7일박준형
    굿게임

---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+
+
+    {
+        public float speed = 10f;
+
+        private void Update()
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);           
+        }
+    }
+
